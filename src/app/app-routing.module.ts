5de539c0d0e5a1fa5c443651c39b156749560262
DanielRY <danielrojasy@upeu.edu.pe>
2020-08-04 14:39:04 -0500

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'home',component:HomeComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CreateComponent } from './componentes/usuario/create/create.component';
+import { EditComponent } from './componentes/usuario/edit/edit.component';
+import { ListComponent } from './componentes/usuario/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CreateComponent,
+    EditComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
